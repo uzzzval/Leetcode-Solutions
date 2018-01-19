af4bd61953d2fc0c588b0ae2525de37d1d0e3941
Ujjawal Sharma <uzzzval@gmail.com>
2018-01-19 17:45:09 -0500

    Create Future.java

---
+++ Future.java
@@ -0,0 +1,31 @@
+public class Future {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int A=4;
+		int K=5;
+		int ahead=1;
+		
+		int result=minNum(A,K,ahead);
+		System.out.println(result);
+	}
+
+	public static int minNum(int A, int K,int ahead) {
+		int count=0;
+		int startingA=0;
+		int startingK=0-ahead;
+		while(true) {
+			if(startingA==0 && startingK==0) {
+				startingA=0;
+				startingK=0-ahead;
+			}
+			startingA=startingA+A;
+			startingK=startingK+K;
+			count++;
+			if(startingK>startingA)
+				break;
+		}
+		return count;
+	}
+
+}

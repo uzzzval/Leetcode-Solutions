65c102636a5d1b82f9e1ccaa989521a38c7768f6
Ujjawal Sharma <uzzzval@gmail.com>
2018-01-20 10:35:03 -0500

    Create TravelPlan.java

---
+++ TravelPlan.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class TravelPlan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] nums= {1,0,0,0,2,7,0,0,0,0,9};
+		int length=nums.length;
+		List<Integer> distinctList=new ArrayList<Integer>();
+		for(int i=0;i<length;i++) {
+			if(!distinctList.contains(nums[i]))
+				distinctList.add(nums[i]);
+		}
+		List<Integer> masterList=distinctList;
+		List<Integer> tempList=new ArrayList<Integer>();
+		int returnRange=0;
+		for(int i=0;i<length;i++) {
+			if(tempList.contains(nums[i])) {
+				Iterator listIterator=tempList.iterator();
+				while(listIterator.hasNext()) {
+					int itValue=(int) listIterator.next();
+					if(itValue==nums[i])
+						listIterator.remove();
+				}
+
+				tempList.add(nums[i]);
+				int size=tempList.size();
+				distinctList.removeAll(tempList);
+				if(distinctList.isEmpty()) {
+					if(returnRange==0)
+						returnRange=size;
+					else if (size<returnRange)
+						returnRange=size;
+					distinctList=masterList;
+				}
+			}else {
+
+				tempList.add(nums[i]);
+				int size=tempList.size();
+				distinctList.removeAll(tempList);
+				if(distinctList.isEmpty()) {
+					if(returnRange==0)
+						returnRange=size;
+					else if (size<returnRange)
+						returnRange=size;
+					distinctList=masterList;
+				}
+			}
+		}
+		System.out.println(returnRange);
+	}
+
+}

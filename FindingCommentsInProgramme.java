d3d0a9e3dfa0b8242693f0f8a0fb4bbf638addda
Ujjawal Sharma <uzzzval@gmail.com>
2018-01-21 22:37:48 -0500

    Create FindingCommentsInProgramme.java

---
+++ FindingCommentsInProgramme.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class FindingCommentsInProgramme {
+	public static void main(String args[] ) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		List<String> input = new ArrayList<>();
+		String line = br.readLine();
+		while (line!=null) {
+			input.add(line);
+			line = br.readLine();
+		}
+
+		List<String> output = new ArrayList<>();
+		for (int i = 0; i < input.size(); i++) {
+			if (input.get(i).indexOf("//") != -1) {
+				output.add(input.get(i).substring(input.get(i).indexOf("//")));
+
+			} else if (input.get(i).indexOf("/*") != -1) {
+				if (input.get(i).indexOf("*/") != -1) {
+					output.add(input.get(i).substring(input.get(i).indexOf("/*"), input.get(i).indexOf("*/") + 2));
+
+				} else {
+					while (input.get(i).indexOf("*/") == -1) {
+						output.add(input.get(i));
+						i++;
+						if (i >= input.size())
+							break;
+					}
+					output.add(input.get(i).substring(0, input.get(i).indexOf("*/") + 2));
+				}
+			}
+		}
+
+		for (String i : output) {
+			System.out.println(i.trim());
+		}
+
+	}
+}

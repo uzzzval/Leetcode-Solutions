89521d4510abf6f6cd48b0c1d650f0f541f83099
Ujjawal Sharma <uzzzval@gmail.com>
2018-01-27 23:14:07 -0500

    Create JudgeRouteCircle.java

---
+++ JudgeRouteCircle.java
@@ -0,0 +1,25 @@
+class JudgeRouteCircle {
+    public boolean judgeCircle(String moves) {
+        Map map=new HashMap();
+        map.put('U',0);
+        map.put('D',0);
+        map.put('R',0);
+        map.put('L',0);
+        for(int i=0;i<moves.length();i++){
+            if(map.get(moves.charAt(i))!=null){
+                int count=Integer.parseInt(map.get(moves.charAt(i)).toString());
+                map.put(moves.charAt(i),count+1);
+            }
+        }
+       
+        int right=Integer.parseInt(map.get('R').toString());
+        int left=Integer.parseInt(map.get('L').toString());
+        int up=Integer.parseInt(map.get('U').toString());
+        int down=Integer.parseInt(map.get('D').toString());
+        
+        if(right==left && up==down)
+           return true;
+        else 
+           return false;
+    }
+}

b4442dd93fc2f1dcc61211be36020c5b8f6db273
Ujjawal Sharma <uzzzval@gmail.com>
2018-01-27 23:13:26 -0500

    Create BinaryNumberWithAlternatingBits.java

---
+++ BinaryNumberWithAlternatingBits.java
@@ -0,0 +1,13 @@
+class BinaryNumberWithAlternatingBits {
+    public boolean hasAlternatingBits(int n) {
+        String binaryNumber=Integer.toBinaryString(n);
+        System.out.println(binaryNumber);
+        char[] binaryArr=binaryNumber.toCharArray();
+        boolean hasAlternate=true;
+        for(int i=0;i<binaryArr.length-1;i++){
+            if(binaryArr[i]==binaryArr[i+1])
+                hasAlternate=false;
+        }
+        return hasAlternate;
+    }
+}

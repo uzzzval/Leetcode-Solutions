b0bba637b96cada4e0f92c1a43ab2633b2bf708c
Ujjawal Sharma <uzzzval@gmail.com>
2018-01-29 07:24:11 -0500

    Create NumberOf1Bits.java

---
+++ NumberOf1Bits.java
@@ -0,0 +1,14 @@
+public class NumberOf1Bits {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        String binaryNumber=Integer.toBinaryString(n);
+        char[] binaryArr=binaryNumber.toCharArray();
+        
+        int count=0;
+        for(int i=0;i<binaryArr.length;i++){
+            if(binaryArr[i]=='1')
+                count++;
+        }
+      return count;  
+    }
+}

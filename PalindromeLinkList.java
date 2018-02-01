3a4eb34f982eeadbde7ccadd4b2968d6175e4677
Ujjawal Sharma <uzzzval@gmail.com>
2018-01-31 23:38:49 -0500

    Create PalindromeLinkList.java

---
+++ PalindromeLinkList.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class PalindromeLinkList {
+    public boolean isPalindrome(ListNode head) {
+        if(head==null)
+            return true;
+        List arrayList=new ArrayList();
+        
+        while(head!=null){
+            arrayList.add(head.val);
+            head=head.next;
+        }
+      
+        int length=arrayList.size();
+        int i=0;
+        int j=length-1;
+        boolean isPalindrome=true;
+        while(i<j){
+          if(Integer.parseInt(arrayList.get(i).toString())!=Integer.parseInt(arrayList.get(j).toString()))
+              isPalindrome=false;
+          i++;
+          j--;
+        }
+        return isPalindrome;
+    }
+}

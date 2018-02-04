84dc34c3fcf84fec6cbb32a4941147540db9257f
Ujjawal Sharma <uzzzval@gmail.com>
2018-02-03 22:06:57 -0500

    Update FindMinimumInRotatedSortedArrayII.java

--- FindMinimumInRotatedSortedArrayII.java
+++ FindMinimumInRotatedSortedArrayII.java
@@ -3,12 +3,10 @@ class FindMinimumInRotatedSortedArrayII {
         int length=nums.length;
         if(length==1) return nums[0];
         if(length==0) return 0;
-        
         List arrayList=new ArrayList();
         for(int i=0;i<length;i++){
             arrayList.add(nums[i]);
         }
-
         Collections.sort(arrayList);
         return Integer.parseInt(arrayList.get(0).toString());
     }

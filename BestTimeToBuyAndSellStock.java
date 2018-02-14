68b5f8a65049ad5fe236576e831bd806a5dc70d6
Ujjawal Sharma <uzzzval@gmail.com>
2018-02-14 08:19:06 -0500

    Create BestTimeToBuyAndSellStock.java

---
+++ BestTimeToBuyAndSellStock.java
@@ -0,0 +1,19 @@
+class BestTimeToBuyAndSellStock {
+    public int maxProfit(int[] prices) {
+    
+        int length=prices.length;
+        if(length==0) return 0;
+        int lowest=prices[0];
+        int diff=0;
+        for(int i=1;i<length;i++){
+            if(prices[i]>lowest){
+                if(prices[i]-lowest>diff){
+                    diff=prices[i]-lowest;
+                }
+            }else{
+                lowest=prices[i];
+            }
+        }
+        return diff;
+    }
+}

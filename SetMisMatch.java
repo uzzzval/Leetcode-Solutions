780f731e3ce9ef74b8cf1c4a0bc0180985224689
Ujjawal Sharma <uzzzval@gmail.com>
2018-02-17 22:42:13 -0500

    Update SetMisMatch.java

--- SetMisMatch.java
+++ SetMisMatch.java
@@ -5,7 +5,6 @@ class SetMisMatch {
         int missing=0;
        
         int[] values=new int[2];
-     
         Arrays.sort(nums);
          for (int i=0;i<nums.length-1;i++){
              if(nums[i]==nums[i+1]){

d73e82f745b4dadbfeb2331c4bdeec8d76f8c2ce
Ujjawal Sharma <uzzzval@gmail.com>
2018-10-16 03:13:16 -0400

    Create Braces.java

---
+++ Braces.java
@@ -0,0 +1,36 @@
+    // Complete the braces function below.
+    static String[] braces(String[] values) {
+        String[] output = new String[values.length];
+        for (int i = 0; i < values.length; i++) {
+            Stack<Character> stack = new Stack<>();
+            String value = values[i];
+            boolean flag = true;
+            for (int j = 0; j < value.length(); j++) {
+                char c = value.charAt(j);
+                if (c == '}' || c == ']' || c == ')') {
+                    if (!stack.isEmpty()) {
+                        if (!((c == ')' && stack.peek() == '(')
+                              || (c == '}' && stack.peek() == '{') 
+                              || (c == ']' && stack.peek() == '['))) {
+                            flag = false;
+                            break;
+                        }else{
+                            stack.pop();
+                        }
+                    }else {
+                        flag = false;
+                        break;
+                    }
+                }else{
+                    stack.push(c);
+                }
+            }
+            if(stack.isEmpty() && flag)
+                output[i] = "YES";
+            else
+                output[i] = "NO";
+        }
+        return output;
+    }
+
+

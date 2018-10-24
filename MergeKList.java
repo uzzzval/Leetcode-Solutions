515ff3b47a305e6d077b577d0a386c2eaa7ad256
Ujjawal Sharma <uzzzval@gmail.com>
2018-10-24 07:05:06 -0400

    Create MergeKList.java

---
+++ MergeKList.java
@@ -0,0 +1,28 @@
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        List<Integer> list = new ArrayList<>();
+        ListNode returnNode = null;
+        ListNode pointerNode = returnNode;
+        for(int i=0;i<lists.length;i++){
+            ListNode listNode = lists[i];
+            while(listNode!=null){
+                list.add(listNode.val);
+                listNode=listNode.next;
+            }
+        }
+        Collections.sort(list);
+        
+        for(int item:list){
+            ListNode local = new ListNode(item);
+            if(returnNode==null){
+              returnNode = local; 
+              pointerNode = returnNode;
+            }  
+            else{
+               returnNode.next = local;
+               returnNode = returnNode.next;      
+            }             
+        }
+        return pointerNode;
+    }
+}

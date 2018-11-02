0624c97991957c024a6b64e049dda400a9cadf5d
Ujjawal Sharma <uzzzval@gmail.com>
2018-11-02 07:19:16 -0400

    Create EfficientSearchOn2DArray.java

---
+++ EfficientSearchOn2DArray.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row = matrix.length - 1;
+        int column = 0;
+        
+        while(row>=0 && column<matrix[0].length){
+            if(matrix[row][column]>target){
+                row--;
+            }else if(matrix[row][column]<target){
+                column++;
+            }else{
+                return true;
+            }
+        }
+        return false;
+    } 
+}

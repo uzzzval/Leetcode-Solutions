b11506be86f4664ed164df40f54bdbb0b1e9d8ea
Ujjawal Sharma <uzzzval@gmail.com>
2018-11-09 23:24:36 -0500

    Create MinAddToValidParentheses.java

---
+++ MinAddToValidParentheses.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minAddToMakeValid(String S) {
+        int balance = 0, local = 0;
+        for(int i=0;i<S.length();i++){
+            if(S.charAt(i)=='(')
+                local++;
+            else if(S.charAt(i)==')'){
+              if(local>0){
+                  local--;
+              }  else{
+                  balance++;
+              }
+            } 
+        }
+        return local+balance;
+    }
+}

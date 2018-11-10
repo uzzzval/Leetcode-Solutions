556d603e76bf517f189a2b7ea8eaee88b6efccb2
Ujjawal Sharma <uzzzval@gmail.com>
2018-11-10 00:58:44 -0500

    Create FindAndReplacePattern.java

---
+++ FindAndReplacePattern.java
@@ -0,0 +1,38 @@
+class Solution {
+    public List<String> findAndReplacePattern(String[] words, String pattern) {
+        Map<Character, Integer> patternMap = new HashMap<>();
+        List<String> returnList = new ArrayList<String>();
+        int count = 0;
+        String patternInt = "";
+        for(int i=0;i<pattern.length();i++){
+            if(patternMap.containsKey(pattern.charAt(i))){
+                patternInt = patternInt + patternMap.get(pattern.charAt(i));
+            }else{
+                count++;
+                patternInt = patternInt + count;
+                patternMap.put(pattern.charAt(i), count);
+            }
+        }
+        
+        for(String word: words){
+           int localCount = 0;
+           String localPattern = "";
+           Map<Character, Integer> localMap = new HashMap<>(); 
+           for(int i=0;i<word.length();i++){
+            if(localMap.containsKey(word.charAt(i))){
+                localPattern = localPattern + localMap.get(word.charAt(i));
+                }else{
+                localCount++;
+                localPattern = localPattern + localCount;
+                localMap.put(word.charAt(i), localCount);
+                 }
+            }
+           
+            if(localPattern.equals(patternInt))
+                returnList.add(word);
+        }
+        
+        
+        return returnList;
+    }
+}

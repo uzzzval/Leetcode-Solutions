1c2c5b7ce3ea7a0aa26fcc11bf7cb5e0585db835
Ujjawal Sharma <uzzzval@gmail.com>
2018-12-11 07:56:54 -0500

    Create MissingNumber.java

---
+++ MissingNumber.java
@@ -0,0 +1,16 @@
+class MissingNumber {
+    public int missingNumber(int[] nums) {
+        int length = nums.length;
+        int returnInt = -1;
+        boolean[] flags = new boolean[length+1];
+        for(int i=0;i<length;i++){
+          int index = nums[i];
+          flags[index] = true;  
+        }
+        for(int i=0;i<flags.length;i++){
+            if(!flags[i])
+                 returnInt = i;
+        }
+        return returnInt;
+    }
+}

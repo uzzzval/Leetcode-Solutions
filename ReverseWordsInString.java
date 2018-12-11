e8aa17980dc4be4950dc7f8fa0419b2ca7e3997d
Ujjawal Sharma <uzzzval@gmail.com>
2018-12-11 07:46:25 -0500

    Create ReverseWordsInString.java

---
+++ ReverseWordsInString.java
@@ -0,0 +1,12 @@
+public class ReverseWordInString {
+    public String reverseWords(String s) {
+         String returnString = "";
+        String[] stringArray = s.split(" ");
+        for(int i=stringArray.length-1;i>=0;i--){
+        	System.out.println(stringArray[i]);
+        	if(!(stringArray[i].trim() + " ").equals(" "))
+        		returnString = returnString + stringArray[i].trim() + " ";
+    }
+        return returnString.trim();
+    }
+}

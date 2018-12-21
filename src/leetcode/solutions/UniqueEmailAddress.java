61cafe71409b5abc864b9cf3aee7e2e73839197f
ujjawalsharma <uzzzval@gmail.com>
2018-12-21 07:46:48 -0500

    UniqueEmailAddress

--- src/leetcode/solutions/UniqueEmailAddress.java
+++ src/leetcode/solutions/UniqueEmailAddress.java
@@ -1,8 +1,33 @@
 package leetcode.solutions;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class UniqueEmailAddress {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+       String[] emails = {"test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"};
+       int count = numUniqueEmails(emails);
+       System.out.println(count);
 	}
-
+	
+	public static int numUniqueEmails(String[] emails) {
+		Set<String> uniqueSet = new HashSet<String>();
+        for(int i=0;i<emails.length;i++){
+            String[] emailSplit = emails[i].split("@");
+            String localString = emailSplit[0];
+            localString = localString.replace(".", "");
+            int plusPosition=0;
+            for(int j=0;j<localString.length();j++) {
+            	if(localString.charAt(j)=='+') {
+            		plusPosition = j;
+            		break;
+            	}
+            }
+            if(plusPosition>0) {
+            	localString = localString.substring(0, plusPosition);
+            }
+            uniqueSet.add(localString + emailSplit[1]);
+        }
+        return uniqueSet.size();
+    }
 }
